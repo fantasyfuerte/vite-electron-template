36dd7f820e51e910150686872c641e2c4c723bfc
Fuerte <leandrofuerte75@gmail.com>
2025-05-03 19:03:12 -0400

    styling template

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,23 +1,11 @@
-import { useState } from "react";
-import "./App.css";
-
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <main className="flex flex-col items-center justify-center h-screen">
+        <h1 className="text-4xl font-semibold">
+          Welcome to Vite + Electron + React + TailwindCSS
+        </h1>
+      </main>
     </>
   );
 }
